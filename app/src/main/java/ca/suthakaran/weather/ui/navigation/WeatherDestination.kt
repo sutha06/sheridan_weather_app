daa09e39144707210b9c1d544fd54e688a7f8604
sutha06 <suthakaran.siva0601@gmail.com>
2023-12-08 22:24:20 -0500

    WeatherDestination file made

---
+++ app/src/main/java/ca/suthakaran/weather/ui/navigation/WeatherDestination.kt
@@ -0,0 +1,8 @@
+package ca.suthakaran.weather.ui.navigation
+
+import ca.suthakaran.weather.R
+
+object WeatherDestination: NavDestination {
+    override val route: String = "weather"
+    override val titleRes: Int = R.string.nav_weather_title
+}
