05614d18c2dd043ea407c41b961e8b520328846b
sutha06 <suthakaran.siva0601@gmail.com>
2023-12-09 00:37:29 -0500

    Davis viewmodel file made

---
+++ app/src/main/java/ca/suthakaran/weather/ui/davis/DavisViewModel.kt
@@ -0,0 +1,18 @@
+package ca.suthakaran.weather.ui.davis
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DavisViewModel @Inject constructor(
+
+):ViewModel() {
+
+
+}
