7dc0d1d7b76d1bf1404826dce60212736cea286d
sutha06 <suthakaran.siva0601@gmail.com>
2023-12-09 00:47:20 -0500

    Trafalgar viewmodel file completed

---
+++ app/src/main/java/ca/suthakaran/weather/ui/traf/TrafViewModel.kt
@@ -0,0 +1,19 @@
+package ca.suthakaran.weather.ui.traf
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TrafViewModel @Inject constructor(
+
+):ViewModel() {
+
+
+
+}
