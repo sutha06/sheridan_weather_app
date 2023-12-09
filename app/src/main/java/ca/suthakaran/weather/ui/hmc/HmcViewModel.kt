377490c65a5d788b7fabdd132ea049c2f782e805
sutha06 <suthakaran.siva0601@gmail.com>
2023-12-09 00:31:53 -0500

    hmc viewmodel file made

---
+++ app/src/main/java/ca/suthakaran/weather/ui/hmc/HmcViewModel.kt
@@ -0,0 +1,13 @@
+package ca.suthakaran.weather.ui.hmc
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HmcViewModel @Inject constructor(
+
+):ViewModel() {
+
+
+}
