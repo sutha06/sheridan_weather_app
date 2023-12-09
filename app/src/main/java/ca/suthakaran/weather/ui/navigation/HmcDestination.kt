a0cc33d53ed3753102a3702512048b4c6f2948e6
sutha06 <suthakaran.siva0601@gmail.com>
2023-12-09 00:28:08 -0500

    hmc destination file made

---
+++ app/src/main/java/ca/suthakaran/weather/ui/navigation/HmcDestination.kt
@@ -0,0 +1,8 @@
+package ca.suthakaran.weather.ui.navigation
+
+import ca.suthakaran.weather.R
+
+object HmcDestination: NavDestination {
+    override val route: String = "HMC"
+    override val titleRes: Int = R.string.nav_hmc_title
+}
--- app/src/main/java/ca/suthakaran/weather/ui/navigation/WeatherDestination.kt
+++
@@ -1,8 +0,0 @@
-package ca.suthakaran.weather.ui.navigation
-
-import ca.suthakaran.weather.R
-
-object WeatherDestination: NavDestination {
-    override val route: String = "weather"
-    override val titleRes: Int = R.string.nav_weather_title
-}
