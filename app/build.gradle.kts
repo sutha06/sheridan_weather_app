31c4ad4308d25f2277d7b5e7d72b1c7d95931833
sutha06 <suthakaran.siva0601@gmail.com>
2023-12-09 20:29:44 -0500

    more changes made

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,52 +55,43 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation (platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation (platform("androidx.compose:compose-bom:2023.05.01"))
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.ui:ui-graphics")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.compose.material3:material3")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation ("androidx.navigation:navigation-compose:2.7.2")
 
-    // needed for viewModel() function
-    //implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    // Needed by Hilt (dependency injection)
+    implementation("com.google.dagger:hilt-android:2.44.2")
+    kapt ("com.google.dagger:hilt-compiler:2.44")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
 
-    // needed for the dependency injection
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+    implementation ("androidx.room:room-runtime:2.5.2")
+    implementation ("androidx.room:room-ktx:2.5.2")
+    annotationProcessor ("androidx.room:room-compiler:2.5.2")
+    kapt ("androidx.room:room-compiler:2.5.2")
 
-    // needed for the navigation with view models
-    val navVersion = "2.7.5"
-    implementation("androidx.navigation:navigation-compose:$navVersion")
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation (platform("androidx.compose:compose-bom:2022.10.00"))
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
+    debugImplementation ("androidx.compose.ui:ui-tooling")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest")
 
-    // needed for the view model per destination, the hiltViewModel() function
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    kapt ("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.5.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
 
-    // need to receive remote data
-    //implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-
-    // needed by the local database, not compatible with ksp
-    val roomVersion="2.6.0"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    //noinspection KaptUsageInsteadOfKsp
-    kapt("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
-
-    // needed to store settings
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-    // display images from web or assets
-    //implementation("io.coil-kt:coil-compose:2.4.0")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
 
 // Allow references to generated code
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id ("com.android.library") version "8.1.1" apply false
 
     // needed for the dependency injection
     id("com.google.dagger.hilt.android") version "2.44.2" apply false
